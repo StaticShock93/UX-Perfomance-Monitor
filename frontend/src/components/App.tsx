6c4c12e843c7651dfe9c3785a1c1778c9093665d
Chacta  Brice <chactaib@gmail.com>
2025-06-16 11:06:58 -0700

    installed missing dependencies

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,15 +1,11 @@
-import AnimationTestBox from './AnimationTestBox';
 import UXMonitorToggle from './UXMonitorToggle';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello UX Monitor</h1>
       {/* Dev-only toggle */}
       {import.meta.env.DEV && <UXMonitorToggle />}
-
 	  {/* ADD ANY ANIMATION TO TEST HERE DIRECTLY */}
-		<AnimationTestBox />
     </div>
   );
 }
